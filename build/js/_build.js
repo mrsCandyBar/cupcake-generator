5cee1bc571bad43a0c4823af798d3246b0771896
mrsCandyBar <candicekbar@gmail.com>
2017-06-26 17:55:58 +0200

    Add bracket notation to build

--- build/js/_build.js
+++ build/js/_build.js
@@ -8,24 +8,24 @@ class Build {
         destination.html(rendered)
         resolve();
       });
-    });
 
+    });
     return content;
   }
 
   randomItem(store) {
-    if (store.builder) {
-      for(let property in store.builder) {
-        let index = _getRandomNumberBetween(store.builder[property]);
+    if (store['builder']) {
+      for(let property in store['builder']) {
+        let index = _getRandomNumberBetween(store['builder'][property]);
         
-        if (store.brief[property] === store.builder[property][index]) {
-          index = _getRandomNumberBetween(store.builder[property]);
+        if (store['brief'][property] === store['builder'][property][index]) {
+          index = _getRandomNumberBetween(store['builder'][property]);
         }
-        store.brief[property] = store.builder[property][index];
+        store['brief'][property] = store['builder'][property][index];
       }
 
-      _isItemTall(store.brief);
-      store.active = '';
+      _isItemTall(store['brief']);
+      store['active'] = '';
       return store;
     }
   }
@@ -37,7 +37,7 @@ function _getRandomNumberBetween(obj) {
 
 function _isItemTall(item) {
 	item.type  = item['icing_type'] === 'swirl' ? 'tall' : 'short';
-	item       = item['icing_type'] === 'swirl' ? item.hasCream = '' : item.hasWafer = '';
+	item       = item['icing_type'] === 'swirl' ? item['hasCream'] = '' : item['hasWafer'] = '';
 }
 
 module.exports = new Build();
